746aa26a3cf55de2a844a8d9975ac487eb7fb33c
alamin200290 <amin.se.aiub@gmail.com>
2025-12-30 14:32:54 +0600

    mvc model adding to project

--- php/user-mgt/controllers/loginCheck.php
+++ php/user-mgt/controllers/loginCheck.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    require_once('../models/userModel.php');
     if(isset($_POST['submit'])){
         $username = $_REQUEST['username'];
         $password = $_REQUEST['password'];
@@ -7,17 +8,12 @@
         if($username == "null" || $password == ""){
             echo "null value!";
         }else{
-
-            $con = mysqli_connect('127.0.0.1', 'root', '', 'webtech');
-            $sql = "select * from users where username='{$username}' and password='{$password}'";
-            $result = mysqli_query($con, $sql);
-            $count = mysqli_num_rows($result);
-
-            if($count == 1){
+            $user = ['username'=> $username, 'password'=> $password];
+            $status = login($user);
+            if($status){
                 setcookie('status', 'true', time()+3000, '/');
                 //$_SESSION['status'] = true;
                 $_SESSION['username'] = $username;
-
                 header('location: ../views/home.php');
             }else{
                 echo "invalid user!";
--- php/user-mgt/controllers/signupCheck.php
+++ php/user-mgt/controllers/signupCheck.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-
+    require_once('../models/userModel.php');
     if(isset($_POST['submit'])){
         $username = $_REQUEST['username'];
         $password = $_REQUEST['password'];
@@ -9,17 +9,10 @@
         if($username == "null" || $password == "" || $email == ""){
             echo "null value!";
         }else{
-            // if(){
-            //     count();
-            // };
-            //$users = [];    
-            //$user = ['username'=>$username, 'password'=>$password, 'email'=>$email];
-            //array_push($users, $user);
-            //$_SESSION['user'] = $user;
-
-            $con = mysqli_connect('127.0.0.1', 'root', '', 'webtech');
-            $sql = "insert into users values(null, '{$username}', '{$password}', '{$email}')";
-            if(mysqli_query($con, $sql)){
+            
+            $user = ['username'=>$username, 'password'=>$password, 'email'=>$email];
+            $status = addUser($user);
+            if($status){
                 header('location: ../views/login.php');
             }else{
                 header('location: ../views/signup.php');
